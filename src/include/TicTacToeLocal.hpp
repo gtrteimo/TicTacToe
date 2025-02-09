12a22d40c0bcdf89d7f47156566939c42a13b602
gtrteimo <gtrteimo@gmail.com>
2025-02-09 20:20:43 +0100

    started working on IO

--- src/include/TicTacToeLocal.hpp
+++ src/include/TicTacToeLocal.hpp
@@ -8,13 +8,16 @@
 class TicTacToeLocal : public TicTacToe {
     private:
         uint16_t boardSize, sequence;
-        uint8_t playerCount;
+        uint8_t playerCount, playerTurn = 0;
         TicTacToe* t;
     public:    
         TicTacToeLocal(uint16_t boardSize, uint16_t sequence, uint8_t playerCount);
-        TicTacToeLocal();
+        TicTacToeLocal(); 
     private:
         uint8_t gameLoop();
-        void printToConsole();
+        void printToConsole(uint8_t width);
+        uint32_t readFromConsole();
+        
+        
 };
     
